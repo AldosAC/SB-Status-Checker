351c40bf78f06cbb395594092704dbc5af611207
Joel Carpenter <aldosac@gmail.com>
2021-03-08 12:03:40 -0800

    Refactors server for MVC

---
+++ amplify/backend/function/sbstatuschecker0f62e3b4/src/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+const morgan = require('morgan');
+const { getStatus } = require('./controllers/requestController.js');
+
+const app = express();
+const PORT = 3000;
+
+app.use(morgan('dev'));
+app.use(awsServerlessExpressMiddleware.eventContext());
+
+app.get(`/api/status`, (req, res) => {
+  res.send(getStatus());
+})
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log(`Unable to connect: ${err}`);
+  } else {
+    console.log(`Server listening on port ${PORT}`);
+  }
+})
+
+module.exports = app
