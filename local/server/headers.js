1f92f7a04455b89de64bd0fda3cf9b2f9ebc749d
Joel Carpenter <aldosac@gmail.com>
2021-03-08 20:34:04 -0800

    Adjusts CORS headers

--- local/server/headers.js
+++ local/server/headers.js
@@ -1,4 +1,4 @@
 module.exports = {
-  'Access-Control-Allow-Origin': 'http://127.0.0.1:8080',
+  'Access-Control-Allow-Origin': 'http://127.0.0.1:8080, https://sbstatus.joelcarpenter.net, https://production.dtrgccqbn88yf.amplifyapp.com/',
   'Access-Control-Allow-Methods': 'GET, POST, DELETE, OPTIONS'
 }
