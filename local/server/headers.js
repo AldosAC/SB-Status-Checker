dc33b2a213258d206ced411a2c2e933ae04c9383
Joel Carpenter <aldosac@gmail.com>
2021-03-09 02:14:09 -0800

    Adds CORS middleware

--- local/server/headers.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  'Access-Control-Allow-Origin': 'http://127.0.0.1:8080, https://sbstatus.joelcarpenter.net, https://production.dtrgccqbn88yf.amplifyapp.com/',
-  'Access-Control-Allow-Methods': 'GET, POST, DELETE, OPTIONS'
-}
